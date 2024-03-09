04b0afe037db476834068e9443d5ba29bd258d7c
Troitsk-Nekrasova <150579855+riscovat@users.noreply.github.com>
2024-03-09 11:22:38 +0300

    Add files via upload

---
+++ data/users_resource.py
@@ -0,0 +1,59 @@
+from flask import jsonify
+from flask_restful import abort, Resource, reqparse
+
+from . import db_session
+from .jobs import Jobs
+
+parser = reqparse.RequestParser()
+parser.add_argument('team_leader', required=True, type=int)
+parser.add_argument('job', required=True)
+parser.add_argument('work_size', required=True, type=int)
+parser.add_argument('collaborators', required=True)
+parser.add_argument('is_finished', required=True, type=bool)
+parser.add_argument('user_id', required=True, type=int)
+
+
+def abort_if_jobs_not_found(jobs_id):
+    session = db_session.create_session()
+    jobs = session.query(Jobs).get(jobs_id)
+    if not jobs:
+        abort(404, message=f"News {jobs_id} not found")
+
+
+class UsersResource(Resource):
+    def get(self, news_id):
+        abort_if_jobs_not_found(news_id)
+        session = db_session.create_session()
+        jobs = session.query(Jobs).get(news_id)
+        return jsonify({'jobs': jobs.to_dict(
+            only=('job', 'team_leader', 'work_size', 'collaborators', 'is_finished', 'user.id'))})
+
+    def delete(self, jobs_id):
+        abort_if_jobs_not_found(jobs_id)
+        session = db_session.create_session()
+        jobs = session.query(Jobs).get(jobs_id)
+        session.delete(jobs)
+        session.commit()
+        return jsonify({'success': 'OK'})
+
+
+class UsersListResource(Resource):
+    def get(self):
+        session = db_session.create_session()
+        news = session.query(News).all()
+        return jsonify({'news': [item.to_dict(
+            only=('title', 'content', 'user.name')) for item in news]})
+
+    def post(self):
+        args = parser.parse_args()
+        session = db_session.create_session()
+        news = News(
+            title=args['title'],
+            content=args['content'],
+            user_id=args['user_id'],
+            is_published=args['is_published'],
+            is_private=args['is_private']
+        )
+        session.add(news)
+        session.commit()
+        return jsonify({'id': news.id})
